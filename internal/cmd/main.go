5eee4047275d6dcc02584febe02d726faca5b598
Lucio Pintanel <lm.pintanel@gmail.com>
2024-10-07 20:22:32 -0300

    - Feito ajuste para inicialização o banco de dados.

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"exemplo.com/crud/internal/database"
 	"exemplo.com/crud/internal/ui"
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 )
 
@@ -9,7 +11,10 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("Product App")
 
+	database.InitDB()
+
 	ui.SetupUI(w)
 
+	w.Resize(fyne.NewSize(400, 400))
 	w.ShowAndRun()
 }
